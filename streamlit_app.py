c29b604259a14a698c30251082090382016f8794
chaeyeon3 <57486316+chaeyeon3@users.noreply.github.com>
2024-02-02 16:42:50 +0900

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit as st
 import os
 
 # Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["key"])
+st.write("DB username:", st.secrets["user"])
+st.write("DB password:", st.secrets["password"])
+st.write("My cool secrets:", st.secrets["connections.snowflake"]["key"])
 
 # And the root-level secrets are also accessible as environment variables:
 st.write(
