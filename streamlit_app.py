c12926435f9665e2cf431650554bfa8d03204fa7
chaeyeon3 <57486316+chaeyeon3@users.noreply.github.com>
2024-02-02 13:56:25 +0900

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import os
 # Everything is accessible via the st.secrets dict:
 st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["key"])
+st.write("My cool secrets:", st.secrets["my_secrets"]["key"])
 
 # And the root-level secrets are also accessible as environment variables:
 st.write(
